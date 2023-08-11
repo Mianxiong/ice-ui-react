e82c416022d1ca1147bf7632237e1645bbe32607
frankxiong <757134184@qq.com>
2023-08-11 21:56:52 +0800

    feature: finish Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
-        <h2>Hello world</h2>
-        <h3>Hello world</h3>
-        <hr/>
-        <code>
-          const a = 'b'
-        </code>
+        <Button className="custom"> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
